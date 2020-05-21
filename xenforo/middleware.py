37f9b8f8474f79d59edb4d9e6d9f7e75ddbe679b
Augusto Destrero <a.destrero@gmail.com>
2020-05-21 10:43:03 +0200

    Use a completely custom user backend, don't rely on RemoteUserBackend

--- xenforo/middleware.py
+++ xenforo/middleware.py
@@ -2,6 +2,8 @@ from time import time
 import logging
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.contrib import auth
 
 from .models import XenforoUser, XenforoSession
 
@@ -17,17 +19,20 @@ class XFSessionMiddleware(object):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        if 'xenforo_username' not in request.session:
+        if 'xf_user_id' not in request.session:
             xf_session_id = request.COOKIES.get(settings.XENFORO['cookie_prefix'] + 'session', None)
-
-            request.xf_session = None
             if xf_session_id:
                 try:
                     xenforosession = XenforoSession.objects.using(settings.XENFORO['database']).get(pk=xf_session_id, expiry_date__gte=int(time()))
                 except XenforoSession.DoesNotExist:
                     pass
                 else:
-                    request.xf_session = xenforosession.get_session_data()
+                    php_session_data = xenforosession.get_session_data()
+                    try:
+                        request.session['xf_user_id'] = int(php_session_data.get(b'userId'))
+                    except:
+                        # not found a valid Xenforo userID in session data. No login!
+                        pass
 
         response = self.get_response(request)
 
@@ -37,7 +42,7 @@ class XFSessionMiddleware(object):
         return response
 
 
-class XFRemoteUserMiddleware(object):
+class XFUserMiddleware(object):
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -47,23 +52,33 @@ class XFRemoteUserMiddleware(object):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        if 'xenforo_username' in request.session:
-            request.META['REMOTE_USER'] = request.session['xenforo_username']
-        else:
-            assert hasattr(request, 'xf_session'), "The XFRemoteUserMiddleware middleware requires the XFSessionMiddleware middleware to be installed."
+        # AuthenticationMiddleware is required so that request.user exists.
+        if not hasattr(request, 'user'):
+            raise ImproperlyConfigured(
+                "The XFUserMiddleware auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the XFUserMiddleware class.")
+        if request.user.is_authenticated:
+            return self.get_response(request)
+
+        # user is not logged in, search for Xenforo session data to get the Xenforo userId in current session
+        if 'xf_user_id' in request.session:
+            # found a Xenforo userID. Lookup the user in Xenforo database user table
             try:
-                lookup_user_id = int(request.xf_session.get(b'userId'))
-            except:
+                xenforouser = XenforoUser.objects.using(settings.XENFORO['database']).get(pk=request.session['xf_user_id'])
+            except XenforoUser.DoesNotExist:
                 pass
             else:
-                try:
-                    xenforouser = XenforoUser.objects.using(settings.XENFORO['database']).get(pk=lookup_user_id)
-                except XenforoUser.DoesNotExist:
-                    pass
-                else:
-                    if xenforouser.user_state == 'valid' and xenforouser.is_banned not in (True, '1'):
-                        request.META['REMOTE_USER'] = xenforouser.username
-                        request.session['xenforo_username'] = xenforouser.username
+                if xenforouser.user_state == 'valid' and xenforouser.is_banned not in (True, '1'):
+                    # attempt to authenticate the user using our UserBackend.
+                    user = auth.authenticate(request, xenforouser=xenforouser)
+                    if user:
+                        # User is valid.  Set request.user and persist user in the session
+                        # by logging the user in.
+                        request.user = user
+                        auth.login(request, user)
 
         response = self.get_response(request)
 
--- xenforo/user_backend.py
+++ xenforo/user_backend.py
@@ -1,6 +1,5 @@
 import logging
 
-from django.contrib.auth.backends import RemoteUserBackend
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
@@ -8,7 +7,7 @@ from .models import XenforoUser
 
 logger = logging.getLogger(__name__)
 
-class UserBackend(RemoteUserBackend):
+class UserBackend(object):
 
     def get_user(self, user_id):
         try:
@@ -17,37 +16,25 @@ class UserBackend(RemoteUserBackend):
         except UserModel.DoesNotExist:
             return None
 
-    def authenticate(self, request, remote_user):
+    def authenticate(self, request, xenforouser):
         """
-        The username passed as ``remote_user`` is considered trusted.  This
-        method simply returns the ``settings.AUTH_USER_MODEL`` object with the given username,
+        The user passed as ``xenforouser`` is considered trusted and it's an instance of models.XenforoUser.
+        This method returns the ``settings.AUTH_USER_MODEL`` object with the given username (and optionally the same xenforo_id),
         creating a new ``settings.AUTH_USER_MODEL`` object if it not already exists.
         """
-        if not remote_user:
-            return
-        username = self.clean_username(remote_user)
         UserModel = get_user_model()
-        lookup_params = { 'username': username }
+        lookup_params = { 'username': xenforouser.username }
+        # if configured to take Xenforo User ID into account, add the id to the lookup fields
+        save_xenforo_user_id = settings.XENFORO.get('save_xenforo_user_id')
+        if save_xenforo_user_id:
+            lookup_params['xenforo_id'] = xenforouser.id
+        # if running with Sites framework turned on, take the Site into account
         if settings.SITE_ID:
             lookup_params['site_id'] = settings.SITE_ID
-        user, created = UserModel.objects.get_or_create(**lookup_params)
-        if created:
-            user = self.configure_user(request, user)
-        return user
-
-    def configure_user(self, request, user):
-        """
-        Get some info from Xenforo user and save them on Django user
-        """
-        try:
-            xenforouser = XenforoUser.objects.using(settings.XENFORO['database']).get(username=user.username)
-        except XenforoUser.DoesNotExist:
-            logger.error('Unable to find Xenforo user with username %s' % user.username)
-        else:
-            user.email = xenforouser.email
-            user.is_superuser = xenforouser.is_admin
-            user.is_staff = xenforouser.is_staff
-            if hasattr(user, 'xenforo_id'):
-                user.xenforo_id = xenforouser.id
-            user.save()
+        user, created = UserModel.objects.get_or_create(**lookup_params,
+            defaults={
+                'email': xenforouser.email,
+                'is_superuser': xenforouser.is_admin,
+                'is_staff': xenforouser.is_staff
+            })
         return user
